6233c6ec6c5eb158ee47f3c24546047d74515f2f
Sanzen_windows <sanzen.tyo@gmail.com>
2022-05-05 17:25:46 +0900

    timeを編集

--- hello/views.py
+++ hello/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 from .models import Greeting
+import os
 
-
-
+def index(request):
+    times = int(os.environ.get('TIMES', 3))
+    return HttpResponse('Hello! ' * times)
+"""
 def index(request):
     r = requests.get('https://httpbin.org/status/418')
     print(r.text)
     return HttpResponse('<pre>' + r.text + '</pre>')
-
+"""
 
 
 def db(request):
